9c42237a8ab733fdd5a710f007bb2316133507cf
santhosh <santhosh.manda7@gmail.com>
2023-12-16 22:52:52 +0530

    End of Section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -11,11 +11,11 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) {
+  constructor() {
 
   }
   ngOnInit(): void {
-    this.getUsers();
+    
   }
 
 
@@ -23,13 +23,7 @@ export class HomeComponent implements OnInit {
     this.registerMode = !this.registerMode;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("request completed")
-    });;
-  }
+
 
   cancelRegisterMode(event:boolean){
     this.registerMode=event;
